6a6805878398e02c96d2b0fef95cef4fe13b59d8
AlexDev404 <alexdev404@users.noreply.github.com>
2024-05-13 22:10:08 -0600

    The database is now stored in `%APPDATA%\GUIFinalProject`

--- database.cpp
+++ database.cpp
@@ -1,10 +1,17 @@
 #include "database.hpp"
+#include <QDebug>
+#include <QStandardPaths>
+#include <QDir>
 
 void database::setDatabase(string db_name) {
 	this->db_name = db_name;
 }
 
 odb::sqlite::database database::getDatabase() {
-	odb::sqlite::database db_context(this->db_name + ".sqlite", SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
+	QString roaming_app_data = QStandardPaths::standardLocations(QStandardPaths::AppDataLocation)[0];
+	QDir dir(roaming_app_data);
+	if (!dir.exists())
+		dir.mkpath(".");
+	odb::sqlite::database db_context(roaming_app_data.toStdString() + "\\" + this->db_name + ".sqlite", SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
 	return db_context;
 }
